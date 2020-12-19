2f3f21aca59b3444dbf1cbdb8beac01b9e52e0ca
thetayloredman <nutdriver716@gmail.com>
2020-12-18 18:16:53 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>we are all pog</h1>
+      <Switch>
+        
+      </Switch>
     </div>
   );
 }
 
 export default App;
-
-
-// Imagine using cSpell 
-// imagine *not* using cSpell
-// Fuck Drago /s
-// Yes~ OWO
-// *saves*
-// ew you mean '.' not '*'
-// git add .
-// git commit -m "Init"
-// git push --force-with-lease
-// What is this yellow square? is it vim o.O
-// O.O you use VSVim dont u
-// Nom  Ig when I'm live sharing ig it shares extentions
-// hmm i dont use vsvim
