ca38dd4c8753ed867314a942b15bcee6388cb1f1
thetayloredman <nutdriver716@gmail.com>
2020-12-18 18:24:52 -0800

    Reformat

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch></Switch>
+        </div>
+    );
 }
 
 export default App;
