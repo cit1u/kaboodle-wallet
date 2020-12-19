297b8ccbfcbb484bf4d7877c53e997bcb43c6506
thetayloredman <nutdriver716@gmail.com>
2020-12-18 17:53:26 -0800

    Use react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>we are all pog</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// Imagine using cSpell 
+// imagine *not* using cSpell
+// Fuck Drago /s
+// Yes~ OWO
+// *saves*
+// ew you mean '.' not '*'
+// git add .
+// git commit -m "Init"
+// git push --force-with-lease
+// What is this yellow square? is it vim o.O
+// O.O you use VSVim dont u
+// Nom  Ig when I'm live sharing ig it shares extentions
+// hmm i dont use vsvim
