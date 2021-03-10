6262fadd34d2dc77f7816c69217c9272800c9b0c
Vulpo <developervulpo@outlook.com>
2021-03-10 10:52:55 -0600

    :eyes::eyes::eyes:

--- src/pages/Root/index.jsx
+++ src/pages/Root/index.jsx
@@ -6,8 +6,8 @@ export function Root() {
     return (
         <div id="root-container">
             <div className="container">
-            <Box title="Project Sharing"><p>Sharing is caring</p></Box>
-            <Box><p>box without title</p></Box>
+            <Box title="Project Sharing"><p className="text-center">Sharing is caring</p></Box>
+            <Box><p>Seems like you don't have a wallet connected <a href="/connect" className="connect">Connect Wallet</a></p></Box>
             <Box><p>box without title</p></Box>
             <Box><p>box without title</p></Box>
             <Box><p>box without title</p></Box>
